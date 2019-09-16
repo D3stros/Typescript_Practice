1860f85a23da7387cdf6b05ac5925cafa78a7768
Destros <patrick.fallmann@hotmail.com>
2019-09-16 18:00:38 +0200

    Access Modifiers in Constructor Parameters

--- main.ts
+++ main.ts
@@ -2,10 +2,7 @@
 class Point {
   // Access modifiers to control access of elements in a class. Here x and y can't be accessed outside.
   // Options are public, private and protected
-  private x: number;
-  private y: number;
-  // Parameters x and y are optional here because of the ?
-  constructor(x?: number, y?: number) {
+  constructor(private x?: number, private y?: number) {
     this.x = x;
     this.y = y;
   }
