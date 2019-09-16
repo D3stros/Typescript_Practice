e5f35c88a171df7e7e813624e3e951dbbbf04ac5
Destros <patrick.fallmann@hotmail.com>
2019-09-16 16:36:54 +0200

    Typescript tyoes and enum

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
-function log(message) {
-  console.log(message);
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, "a", false];
+
+enum Color {
+  Red = 0,
+  Green = 1,
+  Blue = 2,
+  Purple = 3
 }
-
-var message = "Hello World";
-
-log(message);
+let backgroundColor = Color.Red;
